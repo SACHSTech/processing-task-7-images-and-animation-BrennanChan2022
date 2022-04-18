1f3e242acd79c1d1db04f8b7f4b78f04f331da8e
Brennan Chan <99412514+BrennanChan2022@users.noreply.github.com>
2022-04-18 15:34:29 +0000

    upload img

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,25 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
+  // Declare global image cariable
+  PImage imgShell;
+  PImage imgBackground;
+  
+  float fltShellX = random(0, 1000);
+  float fltShellY = random(0, 1000);
+
+  // The amount that the x and y values increase or decrease by
+  float fltSpeedX = 4;
+  float fltSpeedY = 4;
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1000, 1000);
   }
 
   /** 
@@ -16,21 +27,29 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
-  }
+
+    // Load images: Green shell and white background
+    // Shell dimensions: 160 x 140
+    imgShell = loadImage("shell.png"); 
+    imgBackground = loadImage("white.jpg");
+    
+    }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Draw White background
+    image(imgBackground, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // Draw Green Shell
+    image(imgShell, fltShellX, fltShellY);
+
+    // Animate Green Shell
+    fltShellX += fltSpeedX;
+    fltShellY += fltSpeedY;
+
   
   // define other methods down here.
 }
